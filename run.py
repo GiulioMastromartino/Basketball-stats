b376cc7fa3e4226c7411430b98218c60a614dcf6
GiulioMastromartino <151667126+GiulioMastromartino@users.noreply.github.com>
2025-11-30 12:45:14 +0100

    port 8080

--- run.py
+++ run.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 import argparse
+
 from web import create_app
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--env', default='development')
-    parser.add_argument('--host', default='0.0.0.0')
-    parser.add_argument('--port', type=int, default=5000)
+    parser.add_argument("--env", default="development")
+    parser.add_argument("--host", default="0.0.0.0")
+    parser.add_argument("--port", type=int, default=8080)
     args = parser.parse_args()
     app = create_app(args.env)
-    app.run(host=args.host, port=args.port, debug=app.config['DEBUG'])
+    app.run(host=args.host, port=args.port, debug=app.config["DEBUG"])
