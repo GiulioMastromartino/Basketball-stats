e1c9540b2c19e5275ed836635532393d84399ca3
GiulioMastromartino <151667126+GiulioMastromartino@users.noreply.github.com>
2026-01-24 17:24:30 +0100

    Revert web/__init__.py to previous state

--- web/__init__.py
+++ web/__init__.py
@@ -86,14 +86,12 @@ def setup_logging(app: Flask, config):
 def register_blueprints(app: Flask):
     """Register all blueprints"""
     # Import blueprints here to avoid circular imports
-    from web.routes.analytics import analytics_bp
+    from web.routes.analytics import analytics_bp  # <--- NEW IMPORT
     from web.routes.api import api_bp
     from web.routes.auth import auth_bp
     from web.routes.main import main_bp
-    from web.routes.plays import plays_bp
 
     app.register_blueprint(auth_bp, url_prefix="/auth")
     app.register_blueprint(main_bp)
     app.register_blueprint(api_bp, url_prefix="/api/v1")
-    app.register_blueprint(analytics_bp)
-    app.register_blueprint(plays_bp)
+    app.register_blueprint(analytics_bp)  # <--- NEW REGISTRATION
