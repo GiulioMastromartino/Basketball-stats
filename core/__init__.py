814dade63f329e79029c7060497eb50c50ba8cf3
GiulioMastromartino <151667126+GiulioMastromartino@users.noreply.github.com>
2026-04-29 16:47:57 +0200

    PR1: Simplify core/__init__.py to only expose mail extension, removing unused duplicate extension instances

--- core/__init__.py
+++ core/__init__.py
@@ -1,16 +1,8 @@
-import os
-import logging
-from flask import Flask
-from flask_login import LoginManager
-from flask_migrate import Migrate
+"""
+Core package initializer.
+Exposes the Mail extension instance for use throughout the application.
+"""
+
 from flask_mail import Mail
 
-# Import db and bcrypt from models to avoid duplicate instances
-from core.models import db, bcrypt
-
-# Initialize other extensions
-migrate = Migrate()
-login_manager = LoginManager()
 mail = Mail()
-login_manager.login_view = "auth.login"
-login_manager.login_message_category = "info"
